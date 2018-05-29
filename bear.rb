d7f72a934dc3bc1b59fdeeae51ff0d10b7047e41
leeburgess67 <leeburgessmail@gmail.com>
2018-05-29 16:44:49 +0100

    take fish test passed

--- bear.rb
+++ bear.rb
@@ -9,6 +9,15 @@ attr_reader :name
   end
 
 
+  def check_no_of_fish_in_stomach
+    return @stomach.count
+  end
+
+
+  def take_fish_from_river(fish)
+    @stomach << fish
+    return @stomach.count
+  end
 
 
 
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -13,6 +13,17 @@ class BearTest < MiniTest::Test
     assert_equal("Yogi", @bear.name)
   end
 
+  def test_check_fish_in_stomach
+    assert_equal(0, @bear.check_no_of_fish_in_stomach)
+  end
+
+  def test_bear_can_take_fish_from_river
+    @bear.take_fish_from_river(@fish1)
+    assert_equal(1, @bear.check_no_of_fish_in_stomach)
+  end
+
+
+
 
 
 
